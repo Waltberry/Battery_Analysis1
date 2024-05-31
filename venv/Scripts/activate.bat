cb7f19be6c9eba00fa845190553fcb251acfa346
Waltberry <Waltofuzim@gmail.com>
2024-05-31 15:11:23 -0600

    Add .gitignore and requirements.txt added

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\onyero.ofuzim\Desktop\Research work\Data\venv
+set VIRTUAL_ENV=C:\Users\onyero.ofuzim\Desktop\Research work\Battery_Analysis1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
