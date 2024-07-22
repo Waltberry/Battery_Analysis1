0c3b71ad5bd7597c1d3bc8959525449a2f3005b4
Onyero Walter Ofuzim <waltofuzim@gmail.com>
2024-07-22 14:44:19 -0600

    update

--- src/model_fitting.py
+++ src/model_fitting.py
@@ -53,8 +53,34 @@ def fit_model(times, values, n_terms=1, idx=None):
     initial_guess = [0] + [1] * n_terms + [0.1] * n_terms
     
     # Define bounds for the parameters to avoid overflow issues
-    lower_bounds = [-np.inf] + [-np.inf] * n_terms + [0] * n_terms #
-    upper_bounds = [np.inf] + [np.inf] * n_terms + [np.inf] * n_terms #
+    # Define the lower and upper bounds for the parameters in the curve fitting process.
+    # The generalized_exponential_model function has parameters in the following order:
+    # [c1, c2, ..., c_n, b1, b2, ..., b_n]
+    # where c's are the coefficients and b's are the exponents.
+
+    # `lower_bounds` specifies the lower limits for these parameters.
+    # The first parameter (the offset) can be any value, so its lower bound is -infinity.
+    # The next `n_terms` parameters (the c coefficients) can also be any value, so their lower bounds are -infinity.
+    # The last `n_terms` parameters (the b exponents) must be non-negative, so their lower bounds are 0.
+    lower_bounds = [-np.inf] + [-np.inf] * n_terms + [0] * n_terms
+
+    # `upper_bounds` specifies the upper limits for these parameters.
+    # The first parameter (the offset) can be any value, so its upper bound is infinity.
+    # The next `n_terms` parameters (the c coefficients) can also be any value, so their upper bounds are infinity.
+    # The last `n_terms` parameters (the b exponents) can be any value, so their upper bounds are infinity.
+    upper_bounds = [np.inf] + [np.inf] * n_terms + [np.inf] * n_terms
+
+    # We use these bounds in the curve_fit function to constrain the optimization process.
+    # `curve_fit` will try to find the optimal parameters within the specified bounds.
+    
+    '''lower_bounds:
+    The first parameter is allowed to be any real number (-inf).
+    The next n_terms parameters (c coefficients) are also allowed to be any real number (-inf).
+    The last n_terms parameters (b exponents) are constrained to be non-negative (0).
+    
+    upper_bounds:
+    All parameters are allowed to be any real number (inf), meaning there is no upper constraint.'''
+
     
     try:
         # Fit the model
