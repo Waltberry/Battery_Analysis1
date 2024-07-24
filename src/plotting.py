cc982195479519a023aabe75e751dd639e0dd8e3
Onyero Walter Ofuzim <waltofuzim@gmail.com>
2024-07-24 03:01:36 -0600

    updates

--- src/plotting.py
+++ src/plotting.py
@@ -20,7 +20,7 @@ def plot_scatterplot(times, values, idx=None, x_label='Time (hours)', y_label='E
     None
     """
     plt.figure()
-    plt.scatter(times, values, label=f'Charging Cycle {idx+1}' if idx is not None else '')
+    plt.scatter(times, values, label=f'Charging Cycle {idx+1}' if idx is not None else '', alpha=0.5)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     if title:
@@ -30,6 +30,37 @@ def plot_scatterplot(times, values, idx=None, x_label='Time (hours)', y_label='E
     if idx is not None:
         plt.legend()
     plt.show()
+
+def plot_scatterplot_on_ax(ax, times, values, idx=None, x_label='Time (hours)', y_label='Ewe/mV.4', title=None):
+    """
+    Plot data as a scatter plot on the provided axes for a given charging cycle.
+
+    This function creates a scatter plot of the provided time and value data,
+    with customizable x and y labels, and an optional title. If no title is provided,
+    it defaults to 'Charging Cycle {idx+1}' if idx is given, otherwise '{y_label} vs {x_label}'.
+
+    Parameters:
+    ax (matplotlib.axes.Axes): The axes to plot on.
+    times (list or array-like): List or array of time values.
+    values (list or array-like): List or array of corresponding data values.
+    idx (int, optional): Index of the charging cycle.
+    x_label (str): Label for the x-axis. Defaults to 'Time (hours)'.
+    y_label (str): Label for the y-axis. Defaults to 'Ewe/mV.4'.
+    title (str, optional): Title for the plot. Defaults to None.
+
+    Returns:
+    None
+    """
+    ax.scatter(times, values, label=f'Charging Cycle {idx+1}' if idx is not None else '', alpha=0.5)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    if title:
+        ax.set_title(title)
+    else:
+        ax.set_title(f'Charging Cycle {idx+1}' if idx is not None else f'{y_label} vs {x_label}')
+    if idx is not None:
+        ax.legend()
+
 
 def plot_fitted(times, values, y_fitted, idx):
     """
@@ -74,7 +105,7 @@ def print_fitted_params(fitted_params, n_terms=3):
     for label, param in zip(param_labels, fitted_params):
         print(f'{label} = {param:.4f}')
 
-def plot_cost(costs, name='MSE'):
+def plot_cost(costs, name='Cost'):
     """
     Plot the costs of each charging cycle.
 
